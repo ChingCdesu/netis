7f25b9eed81130d17977864214426b82f9e9b68c
_ChingC <chingc.ccmail@gmail.com>
2023-12-21 09:53:13 +0800

    style: split long function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,8 @@ use libn2n::{
 };
 use n2n::{EdgeConfig, EdgeError, DEFAULT_MTU};
 use std::{ffi::CString, mem, ptr, str::FromStr};
+use log::{debug, warn};
+use crate::n2n::load_config;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -49,81 +51,17 @@ fn start_edge(config: EdgeConfig) -> Result<String, EdgeError> {
     unsafe {
         let mut conf: n2n_edge_conf_t = mem::zeroed();
         let mut ec: n2n_tuntap_priv_config_t = mem::zeroed();
-        edge_init_conf_defaults(&mut conf);
-        ec.mtu = DEFAULT_MTU;
-        ec.daemon = 1;
-        platform::init(&mut ec);
 
-        let supernode_addr = CString::new(config.supernode_addr).unwrap();
-        conf.community_name = config.community.as_bytes().try_into().unwrap();
-        edge_conf_add_supernode(std::ptr::addr_of_mut!(conf), supernode_addr.as_ptr());
-
-        if !config.encryption_key.is_none() {
-            let encryption_key = CString::new(config.encryption_key.unwrap()).unwrap();
-            conf.encrypt_key = encryption_key.into_raw();
+        let err = load_config(config, conf, ec).err();
+        if !err.is_none() {
+            Err(err.unwrap())
         }
 
-        if !config.address.is_none() {
-            let address = config.address.unwrap();
-            let mut cidr_str = address.clone();
-            if address.starts_with("static:") {
-                ec.ip_mode = std::mem::transmute(CString::new("static").unwrap().as_bytes());
-                cidr_str = cidr_str.trim_start_matches("static:").to_string();
-            } else if address.starts_with("dhcp:") {
-                ec.ip_mode = std::mem::transmute(CString::new("dhcp").unwrap().as_bytes());
-                cidr_str = cidr_str.trim_start_matches("dhcp:").to_string();
+        if !conf.shared_secret.is_null() {
+            if conf.federation_public_key.is_null() {
+                let mut federation_public_key = [[0u8; 32]; 1];
+                // conf.federation_public_key =
             }
-
-            let cidr = Ipv4Cidr::from_str(&cidr_str).unwrap();
-
-            ec.ip_addr = std::mem::transmute(
-                CString::new(cidr.first_address().to_string())
-                    .unwrap()
-                    .as_bytes(),
-            );
-            ec.netmask =
-                std::mem::transmute(CString::new(cidr.mask().to_string()).unwrap().as_bytes());
-        }
-
-        if !config.mac.is_none() {
-            ec.device_mac = std::mem::transmute::<&[u8], &[i8]>(config.mac.unwrap().as_bytes())
-                .try_into()
-                .unwrap();
-        }
-
-        if !config.cipher.is_none() {
-            let cipher = config.cipher.unwrap();
-            if n2n::EdgeCipher::None == cipher {
-                conf.transop_id = 1;
-            } else if n2n::EdgeCipher::Twofish == cipher {
-                conf.transop_id = 2;
-            } else if n2n::EdgeCipher::AES == cipher {
-                conf.transop_id = 3;
-            } else if n2n::EdgeCipher::ChaCha20 == cipher {
-                conf.transop_id = 4;
-            } else if n2n::EdgeCipher::Speck == cipher {
-                conf.transop_id = 5;
-            } else {
-                return Err(EdgeError::InvalidCipher);
-            }
-        }
-
-        if !config.username.is_none() {
-            conf.dev_desc = std::mem::transmute(config.username.unwrap().as_bytes());
-        }
-
-        if !config.password.is_none() {
-            let mut shared_secret = [[0u8; 32]; 1];
-            let password = CString::new(config.password.unwrap()).unwrap();
-            conf.shared_secret = shared_secret.as_mut_ptr();
-            generate_private_key(shared_secret[0].as_mut_ptr(), password.into_raw());
-        }
-
-        if !config.public_key.is_none() {
-            let mut federation_public_key = [[0u8; 32]; 1];
-            let public_key = CString::new(config.public_key.unwrap()).unwrap();
-            conf.federation_public_key = federation_public_key.as_mut_ptr();
-            ascii_to_bin(federation_public_key[0].as_mut_ptr(), public_key.into_raw());
         }
     }
 
--- src-tauri/src/n2n.rs
+++ src-tauri/src/n2n.rs
@@ -1,5 +1,12 @@
+use std::cell::RefCell;
+use std::ffi::CString;
 use std::fmt::write;
+use std::str::FromStr;
+use cidr::Ipv4Cidr;
+use libn2n::{ascii_to_bin, edge_conf_add_supernode, edge_init_conf_defaults, generate_private_key, n2n_edge_conf_t, n2n_edge_t, n2n_tuntap_priv_config_t};
+use log::warn;
 use serde::{Serialize, Deserialize, ser::Serializer};
+use crate::{n2n, platform};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum EdgeCipher {
@@ -57,3 +64,92 @@ impl Serialize for EdgeError {
 }
 
 pub const DEFAULT_MTU: i32 = 1290;
+
+pub unsafe fn load_config(config: EdgeConfig, mut conf: n2n_edge_conf_t, mut ec: n2n_tuntap_priv_config_t) -> Result<(), EdgeError> {
+    edge_init_conf_defaults(&mut conf);
+    ec.mtu = DEFAULT_MTU;
+    ec.daemon = 1;
+    platform::init(&mut ec);
+
+    let supernode_addr = CString::new(config.supernode_addr).unwrap();
+
+    conf.community_name = [0u8; 20];
+    conf.community_name[..config.community.len()].copy_from_slice(config.community.as_bytes());
+
+    edge_conf_add_supernode(std::ptr::addr_of_mut!(conf), supernode_addr.as_ptr());
+
+    if !config.encryption_key.is_none() {
+        let encryption_key = RefCell::new(CString::new(config.encryption_key.unwrap()).unwrap());
+        conf.encrypt_key = encryption_key.as_ptr().into_raw();
+    }
+
+    if !config.address.is_none() {
+        let address = config.address.unwrap();
+        let mut cidr_str = address.clone();
+        if address.starts_with("static:") {
+            ec.ip_mode = std::mem::transmute(CString::new("static").unwrap().as_bytes());
+            cidr_str = cidr_str.trim_start_matches("static:").to_string();
+        } else if address.starts_with("dhcp:") {
+            ec.ip_mode = std::mem::transmute(CString::new("dhcp").unwrap().as_bytes());
+            cidr_str = cidr_str.trim_start_matches("dhcp:").to_string();
+        }
+
+        let cidr = Ipv4Cidr::from_str(&cidr_str).unwrap();
+
+        ec.ip_addr = std::mem::transmute(
+            CString::new(cidr.first_address().to_string())
+                .unwrap()
+                .as_bytes(),
+        );
+        ec.netmask =
+            std::mem::transmute(CString::new(cidr.mask().to_string()).unwrap().as_bytes());
+    }
+
+    if !config.mac.is_none() {
+        ec.device_mac = std::mem::transmute::<&[u8], &[i8]>(config.mac.unwrap().as_bytes())
+            .try_into()
+            .unwrap();
+    }
+
+    if !config.cipher.is_none() {
+        let cipher = config.cipher.unwrap();
+        if n2n::EdgeCipher::None == cipher {
+            conf.transop_id = 1;
+        } else if n2n::EdgeCipher::Twofish == cipher {
+            conf.transop_id = 2;
+        } else if n2n::EdgeCipher::AES == cipher {
+            conf.transop_id = 3;
+        } else if n2n::EdgeCipher::ChaCha20 == cipher {
+            conf.transop_id = 4;
+        } else if n2n::EdgeCipher::Speck == cipher {
+            conf.transop_id = 5;
+        } else {
+            return Err(EdgeError::InvalidCipher);
+        }
+    }
+
+    if !config.username.is_none() {
+        conf.dev_desc = std::mem::transmute(config.username.unwrap().as_bytes());
+    }
+
+    if !config.password.is_none() {
+        let mut shared_secret = [[0u8; 32]; 1];
+        let password = CString::new(config.password.unwrap()).unwrap();
+        conf.shared_secret = shared_secret.as_mut_ptr();
+        generate_private_key(shared_secret[0].as_mut_ptr(), password.into_raw());
+    }
+
+    if !config.public_key.is_none() {
+        let mut federation_public_key = [[0u8; 32]; 1];
+        let public_key = CString::new(config.public_key.unwrap()).unwrap();
+        conf.federation_public_key = federation_public_key.as_mut_ptr();
+        ascii_to_bin(federation_public_key[0].as_mut_ptr(), public_key.into_raw());
+    }
+
+    if conf.transop_id == 1 && !conf.encrypt_key.is_null() {
+        warn!("switching to AES as key was provided");
+        conf.transop_id = 3;
+    }
+
+    Ok(())
+}
